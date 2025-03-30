91881fcacd17fbf6c0b6c24d935b555dc9a7a4df
kwchun0520 <ckw05200@gmail.com>
2025-03-30 17:11:29 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -x
 export $(cat .env | xargs)
-BUCEKT=$1
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 gcloud functions deploy gcs-trigger \
   --gen2 \
-  --memory=128MiB \
-  --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
-  --trigger-event-filters="bucket=$BUCKET" \
+  --memory 128MiB \
+  --trigger-event google.storage.object.finalize \
+  --trigger-resource $1 \
   --entry-point=main \
   --runtime python39 \
-  --region us-central1
+  --region us-central1 

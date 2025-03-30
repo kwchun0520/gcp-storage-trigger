0095fa628a559fcacfb26015413bd99e5c0f82ad
kwchun0520 <ckw05200@gmail.com>
2025-03-30 18:04:43 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,26 @@ gcloud functions deploy gcs-trigger \
   --entry-point=main \
   --runtime python39 \
   --region us-central1 
+
+
+
+
+  # gcloud run deploy gcs-trigger \
+  #   --base-image python:3.9 \
+  #   --platform managed \
+  #   --source . \
+  #   --region us-central1 \
+  #   --cpu 1 \
+  #   --memory 256Mi \
+  #   --set-env-vars 'ENV=prod, VAR=1' \
+  #   # --env-vars-file 'deployment/app.yaml'
+
+
+
+  # gcloud eventarc triggers create TRIGGER_NAME  \
+  #   --location=${REGION} \
+  #   --destination-run-service=helloworld-events  \
+  #   --destination-run-region=${REGION} \
+  #   --event-filters="type=google.cloud.storage.object.v1.finalized" \
+  #   --event-filters="bucket=PROJECT_ID-bucket" \
+  #   --service-account=PROJECT_NUMBER-compute@developer.gserviceaccount.com
